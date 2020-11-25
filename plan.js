6f5c4309ac61235464827970219f5481cbc06427
Rickovald <73193802+Rickovald@users.noreply.github.com>
2020-11-25 16:45:51 +0300

    Downloaded files

---
+++ plan.js
@@ -0,0 +1,44 @@
+odd =
+	`Coding until 10:30<br>
+	Gaming break until 11:00<br>
+	Coding again until 12:00<br>
+	Lunch from 12:00 to 12:30<br><br>
+
+	Learn chineese until 14:00<br>
+	Coffee/shop break for 30 minutes<br>
+	Physycal activities until 17:00<br>
+	Gaming break until 18:00<br><br>
+
+	Coding until 20:00<br>
+	Dinner and free time until 22:00<br>`,
+even =
+	`Physycal ex until 10:30<br>
+	Gaming break until 11:00<br>
+	Learn chineese until 12:00<br>
+	Lunch from 12:00 to 12:30<br>
+
+	Coding until 14:00<br>
+	Coffee/shop break for 30 minutes<br>
+	Physycal activities until 15:30<br>
+	Coding until 17:00<br>
+	Gaming break until 18:00<br><br>
+
+	Coding until 20:00<br>
+	Dinner and free time until 22:00<br>`,
+sun =
+	`Extend gym to 10:00<br>
+	Gaming break until 11:00<br>
+	Chill until 12:00<br>
+	Lunch from 12:00 to 12:30<br><br>
+
+	Learn chineese until 13:30<br>
+	Coding until 15:30<br><br>
+	
+	Dinner and free time until 22:00<br>`,
+morning = 
+	`Wake up at 8:00<br>
+	Gym from 8:10 to 8:40<br>
+	Breakfast from 8:40 to 9:00<br><br>`,
+evening =
+	`Shower for a hour<br>
+	Time to sleep<br><br><br><br>`;
---
+++ script.js
@@ -0,0 +1,60 @@
+﻿let weekDayElements = [
+	sunday = document.createElement ('div'),
+	monday = document.createElement ('div'),
+	tuesday = document.createElement ('div'),
+	wednesday = document.createElement ('div'),
+	thursday = document.createElement ('div'),
+	friday = document.createElement ('div'),
+	saturday = document.createElement ('div')
+];
+function timeIs () {
+	today = new Date(),
+	dayWeek = today.getDay();
+};
+timeIs ();
+setInterval(timeIs,1);
+
+monday.id = wednesday.id = friday.id = 
+tuesday.id = thursday.id = saturday.id =
+sunday.id = 'toDoToday';
+
+monday.innerHTML = wednesday.innerHTML = friday.innerHTML += morning + odd + evening;
+tuesday.innerHTML = thursday.innerHTML = saturday.innerHTML += morning + even + evening;
+sunday.innerHTML += morning + sun + evening;
+
+function refreshing (){
+	main.innerHTML = "";
+	if (dayWeek == 1){
+		main.innerHTML += "It's monday <br>"
+		main.appendChild(weekDayElements[1]);
+	} else if (dayWeek == 2){
+		main.innerHTML += "It's tuesday <br>"
+		main.appendChild(weekDayElements[2]);
+	} else if (dayWeek == 3){
+		main.innerHTML += "It's wednesday my dudes <br>"
+		main.appendChild(weekDayElements[3]);
+	} else if (dayWeek == 4){
+		main.innerHTML += "It's thursday <br>"
+		main.appendChild(weekDayElements[4]);
+	} else if (dayWeek == 5){
+		main.innerHTML += "It's friday <br>"
+		main.appendChild(weekDayElements[5]);
+	} else if (dayWeek == 6){
+		main.innerHTML += "It's saturday <br>"
+		main.appendChild(weekDayElements[6]);
+	} else if (dayWeek == 0) {
+		main.innerHTML += "It's sunday ಠ_ಠ <br>"
+		sunSection.appendChild(weekDayElements[0]);
+	}
+}
+function atMidNight () {
+	hour = today.getHours(),
+	minutes = today.getMinutes(),
+	seconds = today.getSeconds();
+	if (hour == 0 && minutes == 0 && seconds == 0) {
+		refreshing ();
+	};
+}
+refreshing ();
+atMidNight ();
+setInterval(atMidNight, 1000);
